6247e40e798600c859166cbe1984bd25febc5e3a
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-03-12 01:57:01 +0530

    added networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
 require("@openzeppelin/hardhat-upgrades");
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: `https://matic-mumbai.chainstacklabs.com`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
 };
